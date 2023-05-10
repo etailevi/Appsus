1bed9b1f525a582459d1690cb3c21a891ab6f65a
Etai Levi <etai3000@gmail.com>
2023-05-10 13:38:28 +0300

    Added main note cmps

---
+++ apps/note/cmps/note-preview.jsx
@@ -0,0 +1,24 @@
+const { useEffect, useState } = React
+const { Link } = ReactRouterDOM
+
+import { noteService } from "../services/note.service.js"
+import { NoteTxt } from "../cmps/note-txt.jsx"
+import { NoteImg } from "../cmps/note-img.jsx"
+import { NoteVideo } from "../cmps/note-video.jsx"
+import { NoteTodos } from "../cmps/note-todos.jsx"
+
+
+export function NotePreview() {
+
+
+
+
+    return (
+        <section>
+            <NoteTxt />
+            <NoteImg />
+            <NoteVideo />
+            <NoteTodos />
+        </section>
+    )
+}
