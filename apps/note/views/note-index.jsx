150ba86691c772df14378ee9cb5f45dbbb5fe0c5
Etai Levi <etai3000@gmail.com>
2023-05-11 00:52:27 +0300

    Design Note-Index

--- apps/note/views/note-index.jsx
+++ apps/note/views/note-index.jsx
@@ -41,18 +41,22 @@ export function NoteIndex() {
     return (
         <section className="note-index full main-layout">
             {!visible &&
-                <ul className="note-input flex" >
-                    <input type="text" name="" id="" placeholder="Enter a note.." />
-                    <li>
-                        <img src="../../../assets/img/imgs-notes/input-check.svg" alt="" />
-                        <img src="../../../assets/img/imgs-notes/input-brush.svg" alt="" />
-                        <img src="../../../assets/img/imgs-notes/input-image.svg" alt="" />
-                    </li>
+                <ul className="note-input clean-list flex align-center justify-center" >
+                    <div class="add-note-bar">
+                        <li>
+                            <input type="text" name="" id="" placeholder="Add a new note here..." />
+                        </li>
+                        <li className="add-note-opts flex row align-center">
+                            <button><img src="../../../assets/img/imgs-notes/input-check.svg" alt="input-check" /></button>
+                            <button><img src="../../../assets/img/imgs-notes/input-brush.svg" alt="input-brush" /></button>
+                            <button><img src="../../../assets/img/imgs-notes/input-image.svg" alt="input-image" /></button>
+                        </li>
+                    </div>
                 </ul>}
             {visible && <NoteAdd />}
             <NoteFilter DynmicCmp onSetFilter={onSetFilter} filterBy={filterBy} />
             <NoteList notes={notes} onRemoveNote={onRemoveNote} />
-        </section>
+        </section >
     )
 }
 
