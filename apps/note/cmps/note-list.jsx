ef1f8aec36a695303b0fc8ee92b83dc734da2196
Etai Levi <etai3000@gmail.com>
2023-05-10 14:17:45 +0300

    Changes in notes cmps

--- apps/note/cmps/note-list.jsx
+++ apps/note/cmps/note-list.jsx
@@ -1,3 +1,22 @@
-export function NoteList() {
-    return <div>note list</div>
+const { Link } = ReactRouterDOM
+
+import { NotePreview } from "../cmps/note-preview.jsx"
+
+export function NoteList({ notes, onRemoveNote }) {
+
+    return (
+        <ul className="note-list">
+            {notes.map(note =>
+                <li key={note.id}>
+                    <NotePreview note={note} />
+                    <section>
+                        <button onClick={() => onRemoveNote(note.id)} >Remove Note</button>
+                        <button><Link to={`/note/${note.id}`} >Details</Link ></button>
+                        <button><Link to={`/note/edit/${note.id}`} >Edit</Link></button>
+
+                    </section>
+                </li>
+            )}
+        </ul>
+    )
 }
--- apps/note/views/note-index.jsx
+++ apps/note/views/note-index.jsx
@@ -1,23 +1,48 @@
+const { useEffect, useState } = React
+const { Link } = ReactRouterDOM
 
-import { NotePreview } from "../cmps/note-preview.jsx"
+import { noteService } from "../services/note.service.js"
+import { showSuccessMsg } from "../../../services/event-bus.service.js"
+import { NoteList } from "../cmps/note-list.jsx"
 
 export function NoteIndex() {
 
+    const [notes, setNotes] = useState([])
+
+    useEffect(() => {
+        loadNotes()
+    }, [])
+
+    function loadNotes() {
+        noteService.query(setNotes)
+    }
+
+    function handleChange() {
+
+    }
+
+    function onAddNote() {
+
+    }
+
+    function onRemoveNote(noteId) {
+        noteService.remove(noteId).then(() => {
+            const updatedNotes = notes.filter(note => note.id !== noteId)
+            setNotes(updatedNotes)
+            showSuccessMsg(`Note removed!`)
+
+        })
+    }
 
     return (
         <section>
             <form onSubmit={onAddNote}>
                 <label htmlFor="title"></label>
-                <input value={title} onChange={handleChange} name="title" id="title" type="text" placeholder="" />
-
-                <label htmlFor="maxPrice">Max Price:</label>
-                <input value={maxPrice} onChange={handleChange} type="number" name="maxPrice" id="maxPrice" placeholder="By Max Price" />
-
+                <input value="" onChange={handleChange} name="title" id="title" type="text" placeholder="Enter your text here" />
                 <button>Add Note</button>
             </form>
+            <NoteList notes={notes} onRemoveNote={onRemoveNote} />
 
-
-            <NotePreview />
         </section>
     )
 }
