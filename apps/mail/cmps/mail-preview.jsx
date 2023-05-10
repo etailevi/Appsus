ac8271a5f083a0a0265a76689d2a3355c002b320
Steveb599 <Steveb599@gmail.com>
2023-05-10 20:57:57 +0300

    changes in mail-preview.jsx

--- apps/mail/cmps/mail-preview.jsx
+++ apps/mail/cmps/mail-preview.jsx
@@ -8,22 +8,28 @@ export function MailPreview({ mail }) {
     const mailText = mail.body.substring(0, 30)
     return (
         <article className="mail-preview flex space-between">
-            <input type="checkbox" name="first-checkbox" id="" />
-            <img src="../../../assets/img/imgs-gmail/star.svg" alt="" />
-            <img src="../../../assets/img/imgs-gmail/label-important.svg" alt="" />
-            <div className="mail-preview-content">
-                <Link to={`/mail/${id}`} className="flex row">
-                    <h3>{name}</h3>
-                    <h3>{subject}</h3>
-                    <h5>{mailText}</h5>
-                </Link>
-            </div>
-            <h3>{utilService.intToFormat(sentAt)}</h3>
-            {/* Add that buttons will be shown only once mail is hovered */}
-            <img src="../../../assets/img/imgs-gmail/archive-mail.svg" alt="" />
-            <img onClick={() => onRemoveMail(mail.id)} src="../../../assets/img/imgs-gmail/delete-mail.svg" alt="" />
-            <img src="../../../assets/img/imgs-gmail/mark-as-read.svg" alt="" />
-            <img src="../../../assets/img/imgs-gmail/schedule.svg" alt="" />
+            <ul className="clean-list flex space-between row">
+                <li>
+                    <input type="checkbox" name="first-checkbox" id="" />
+                    <img src="../../../assets/img/imgs-gmail/star.svg" alt="" />
+                    <img src="../../../assets/img/imgs-gmail/label-important.svg" alt="" />
+                </li>
+                <li className="mail-preview-content">
+                    <Link to={`/mail/${id}`} className="flex row">
+                        <li className="mail-sender-name"><h3>{name}</h3></li>
+                        <h3>{subject}</h3>
+                        <h5>{mailText}</h5>
+                    </Link>
+                </li>
+                <li className="flex row">
+                    <h3>{utilService.intToFormat(sentAt)}</h3>
+                    {/* Add that buttons will be shown only once mail is hovered */}
+                    <img src="../../../assets/img/imgs-gmail/archive-mail.svg" alt="" />
+                    <img onClick={() => onRemoveMail(mail.id)} src="../../../assets/img/imgs-gmail/delete-mail.svg" alt="" />
+                    <img src="../../../assets/img/imgs-gmail/mark-as-read.svg" alt="" />
+                    <img src="../../../assets/img/imgs-gmail/schedule.svg" alt="" />
+                </li>
+            </ul>
         </article>
     )
 }
