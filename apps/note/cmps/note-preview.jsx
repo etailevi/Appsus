d94e647b14fc1527bb1a79809e271dd701434faf
Etai Levi <etai3000@gmail.com>
2023-05-11 12:56:02 +0300

    Design icons in the note field

--- apps/note/cmps/note-preview.jsx
+++ apps/note/cmps/note-preview.jsx
@@ -26,11 +26,15 @@ export function NotePreview({ note, onRemoveNote }) {
             <article className="note-card">
                 {!!note.info && <h1>{note.info.title}</h1>}
                 {!!note.info && <p>{note.info.txt}</p>}
-                <button className="note-trash" onClick={() => onRemoveNote(note.id)} ><img src="./assets/img/imgs-notes/trash.svg" alt="Trash" /></button>
-                <button className="edit-note" onClick={() => onPinNote(note.id)} ><img src="./assets/img/imgs-gmail/edit.svg" alt="Edit-Note" /></button>
+                <button className="note-pin" onClick={() => onPinNote(note.id)} ><img src="./assets/img/imgs-notes/pin.svg" alt="Pin Note" /></button>
+                <ul>
+                    <li className="flex row">
+                <button className="edit-note" onClick={() => onPinNote(note.id)} ><img src="./assets/img/imgs-notes/edit.svg" alt="Edit-Note" /></button>
                 <button className="mail-send" onClick={() => onPinNote(note.id)} ><img src="./assets/img/imgs-gmail/mail.svg" alt="Mail" /></button>
                 <button className="palette-color" onClick={() => onPinNote(note.id)} ><img src="./assets/img/imgs-notes/color-palette.svg" alt="Color Palette" /></button>
-                <button className="note-pin" onClick={() => onPinNote(note.id)} ><img src="./assets/img/imgs-notes/pin.svg" alt="Pin Note" /></button>
+                <button className="note-trash" onClick={() => onRemoveNote(note.id)} ><img src="./assets/img/imgs-notes/trash.svg" alt="Trash" /></button>
+                    </li>
+                </ul>
 
             </article>
             <NoteTxt />
