d3af9e246f6c203b6dabe2a141440d04de4404ee
Etai Levi <etai3000@gmail.com>
2023-05-12 16:33:05 +0300

    Change layout of note list

--- apps/mail/views/mail-index.jsx
+++ apps/mail/views/mail-index.jsx
@@ -68,7 +68,7 @@ export function MailIndex() {
             <MailFilter onSetFilter={onSetFilter} filterBy={filterBy} />
             <ul className="clean-list flex row">
                 <li className="filter-btns flex column align-center">
-                    <button><Link to={`/mail/starred-mails`} ><img src="./assets/img/imgs-gmail/starred.svg" alt=""/>Starred</Link ></button>
+                    <button><Link to={`/mail/starred-mails`} ><img src="./assets/img/imgs-gmail/star-fill.svg" alt=""/>Starred</Link ></button>
                     <button><Link to={`/mail/important`} ><img src="./assets/img/imgs-gmail/label-important.svg" alt="" />Important</Link ></button>
                     <button><Link to={`/mail/archive`} ><img src="./assets/img/imgs-gmail/archive-mail.svg" alt="archive mail" />Archive</Link ></button>
                     <button><Link to={`/mail/trash`} ><img src="./assets/img/imgs-gmail/delete-mail.svg" alt="delete mail" />Trash</Link ></button>
--- apps/note/cmps/note-list.jsx
+++ apps/note/cmps/note-list.jsx
@@ -4,7 +4,7 @@ import { NotePreview } from "../cmps/note-preview.jsx"
 
 export function NoteList({ notes, onRemoveNote }) {
     return (
-        <ul className="clean-list columns">
+        <ul className="note-container clean-list columns">
             {notes.map(note =>
                 <li key={note.id} className="note-item">
                     <section>
