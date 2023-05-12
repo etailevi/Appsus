5019ff1d2937b1e56e114d106e2d2a83a14e68ae
Etai Levi <etai3000@gmail.com>
2023-05-12 18:02:21 +0300

    Minor changes

--- apps/mail/views/mail-index.jsx
+++ apps/mail/views/mail-index.jsx
@@ -70,12 +70,12 @@ export function MailIndex() {
             <MailFilter onSetFilter={onSetFilter} filterBy={filterBy} />
             <ul className="clean-list flex row">
                 <li className="filter-btns flex column align-center">
-                    <button><Link to={`/mail/starred-mails`} ><img src="./assets/img/imgs-gmail/star-fill.svg" alt="" />Starred</Link ></button>
-                    <button><Link to={`/mail/important`} ><img src="./assets/img/imgs-gmail/label-important.svg" alt="" />Important</Link ></button>
-                    <button><Link to={`/mail/archive`} ><img src="./assets/img/imgs-gmail/archive-mail.svg" alt="archive mail" />Archive</Link ></button>
-                    <button><Link to={`/mail/sent`} >Sent</Link ></button>
-                    <button><Link to={`/mail/draft`} ><img src="./assets/img/imgs-gmail/delete-mail.svg" alt="delete mail" />Draft</Link ></button>
-                    <button><Link to={`/mail/trash`} ><img src="./assets/img/imgs-gmail/delete-mail.svg" alt="delete mail" />Trash</Link ></button>
+                    <button><Link to={`/mail/starred-mails`} ><img src="./assets/img/imgs-gmail/star-fill.svg" alt="Starred mails" />Starred</Link ></button>
+                    <button><Link to={`/mail/important`} ><img src="./assets/img/imgs-gmail/label-important.svg" alt="Important mails" />Important</Link ></button>
+                    <button><Link to={`/mail/archive`} ><img src="./assets/img/imgs-gmail/archive-mail.svg" alt="Archive mails" />Archive</Link ></button>
+                    <button><Link to={`/mail/sent`} ><img src="./assets/img/imgs-gmail/sent.svg" alt="Sent mails" />Sent</Link ></button>
+                    <button><Link to={`/mail/draft`} ><img src="./assets/img/imgs-gmail/draft.svg" alt="Drafts" />Draft</Link ></button>
+                    <button><Link to={`/mail/trash`} ><img src="./assets/img/imgs-gmail/delete-mail.svg" alt="Deleted mail" />Trash</Link ></button>
                 </li>
                 <li>
                     <MailList mails={mails} onRemoveMail={onRemoveMail} onUpdateMail={onUpdateMail} />
