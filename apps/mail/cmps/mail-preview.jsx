b0c29c901f4041272847b2987b84ef4d83e16972
Steveb599 <Steveb599@gmail.com>
2023-05-13 22:16:33 +0300

    Delivery saturday

--- apps/mail/cmps/mail-preview.jsx
+++ apps/mail/cmps/mail-preview.jsx
@@ -4,19 +4,22 @@ const { Link } = ReactRouterDOM
 
 export function MailPreview({ mail, onRemoveMail, onScheduleMail, onUpdateMail }) {
 
-    let isRead = mail.isRead ? 'read' : ''
+    let readImg = mail.isRead ? 'read' : ''
     let readEnvelope = mail.isRead ? "./assets/img/imgs-gmail/closed-envelope.svg" : "./assets/img/imgs-gmail/mark-as-read.svg"
+    let starImg = mail.isStared ? "./assets/img/imgs-gmail/star-fill.svg" : "./assets/img/imgs-gmail/star.svg"
+    let archiveImg = mail.isArchive ? "./assets/img/imgs-gmail/archive-fill.svg" : "./assets/img/imgs-gmail/archive-mail.svg"
+    let importantImg = mail.isImportant ? "./assets/img/imgs-gmail/label-important-fill.svg" : "./assets/img/imgs-gmail/label-important.svg"
 
     const { subject, from, sentAt, name, id } = mail
     const mailText = mail.body ? mail.body.substring(0, 30) : ''
     return (
         <article className="mail-preview">
             <hr />
-            <ul className="clean-list flex align-center row">
+            <ul className="read clean-list flex align-center row">
                 <li className="flex row">
                     <input type="checkbox" name="first-checkbox" id="" />
-                    <img onClick={() => onUpdateMail(mail, 'isStared')} src="./assets/img/imgs-gmail/star.svg" alt="" />
-                    <img onClick={() => onUpdateMail(mail, 'isImportant')} src="./assets/img/imgs-gmail/label-important.svg" alt="" />
+                    <img onClick={() => onUpdateMail(mail, 'isStared')} src={starImg} alt="" />
+                    <img onClick={() => onUpdateMail(mail, 'isImportant')} src={importantImg} alt="" />
                 </li>
                 <li className="mail-preview-content">
                     <Link to={`/mail/${id}`} className="flex row align-center">
@@ -28,7 +31,7 @@ export function MailPreview({ mail, onRemoveMail, onScheduleMail, onUpdateMail }
                 <li className="mail-opts flex row">
                     <h3 className="date-recieved">{utilService.intToFormat(sentAt)}</h3>
                     {/* Add that buttons will be shown only once mail is hovered */}
-                    <img className="show-hovered" onClick={() => onUpdateMail(mail, 'isArchive')} src="./assets/img/imgs-gmail/archive-mail.svg" alt="archive mail" />
+                    <img className="show-hovered" onClick={() => onUpdateMail(mail, 'isArchive')} src={archiveImg} alt="archive mail" />
                     <img className="show-hovered" onClick={() => onRemoveMail(mail.id)} src="./assets/img/imgs-gmail/delete-mail.svg" alt="delete mail" />
                     <img className="show-hovered" onClick={() => onUpdateMail(mail, 'isRead')} src={readEnvelope} alt="mark mail as read" />
                 </li>
