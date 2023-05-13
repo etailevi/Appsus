f11bc97a79446590160280baf783bbf86befd344
Etai Levi <etai3000@gmail.com>
2023-05-13 20:51:25 +0300

    Solved an issue with fonts

--- apps/note/services/note.service.js
+++ apps/note/services/note.service.js
@@ -201,7 +201,7 @@ function getEmptyNote() {
         type: 'NoteTxt',
         isPinned: false,
         style: {
-            backgroundColor: 'pink'
+            backgroundColor: '#F9DEFF'
         },
         info: {
             title: '',
